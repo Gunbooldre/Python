c61a56f696ace927892167e837fe482efd44e855
diasmuratbayev <gunbooldre@gmail.com>
2024-10-21 16:09:20 +0500

    feat

--- fastProject/main.py
+++ fastProject/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
-from fastapi.params import Body
-
+from random import randrange
 from schema import Post
 
 app = FastAPI()
+my_posts = [{"title": "titile of post1 ", "body": "Body of posts 1", "id": 1},
+            {"title": "titile of post2 ", "body": "Body of posts 2", "id": 2}]
+
+
+def find_post(id):
+    for i in my_posts:
+        if i["id"] == id:
+            return i
 
 
 @app.get("/")
@@ -13,10 +20,18 @@ def root():
 
 @app.get("/posts")
 async def get_posts():
-    return {"data": "Here your posts"}
+    return {"data": my_posts}
 
 
 @app.post("/createposts")
 def create_post(data: Post):
-    print(f"new Post {data}")
-    return {"New post": "New post"}
+    data = data.dict()
+    data['id'] = randrange(0, 100000)
+    my_posts.append(data)
+    return {"New post": data}
+
+
+@app.get('/posts/{id}')
+def get_post(id: int):
+    data = find_post(int(id))
+    return ({"post_detail": data})
--- fastProject/schema.py
+++ fastProject/schema.py
@@ -1,6 +1,9 @@
 from pydantic import BaseModel
+from typing import Optional
 
 
 class Post(BaseModel):
     title: str
     body: str
+    published: bool = False
+    rating: Optional[int] = None
