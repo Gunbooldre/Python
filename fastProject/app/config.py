1d57caeae758ee59e63fe965ea425c81c68d45e3
diasmuratbayev <gunbooldre@gmail.com>
2024-12-03 14:37:23 +0500

    feat/adding feat for docker-compsoe

--- fastProject/app/config.py
+++ fastProject/app/config.py
@@ -10,6 +10,9 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
 
     class Config:
         env_file = "./app/.env"
