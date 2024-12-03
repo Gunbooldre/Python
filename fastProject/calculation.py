ff7a5ff818ca7b57b1abddade69733cabd09dd14
diasmuratbayev <gunbooldre@gmail.com>
2024-12-03 19:59:47 +0500

    feat/starting test

---
+++ fastProject/calculation.py
@@ -0,0 +1,28 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
+
+
+class BanckAccount:
+    def __init__(self, starting_balance=0):
+        self.balance = starting_balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        self.balance -= amount
+
+    def collect_interest(self):
+        self.balance *= 1.1
---
+++ fastProject/test/test_calculation.py
@@ -0,0 +1,55 @@
+from calculation import add, subtract, multiply, divide, BanckAccount
+import pytest
+
+
+@pytest.mark.parametrize("num1, num2, result", [
+    (3, 2, 5),
+    (7, 1, 8),
+    (12, 4, 16)
+])
+def test_add(num1, num2, result):
+    print("Testing add function")
+    assert add(num1, num2) == result
+
+
+def test_subtract():
+    print("Testing subtract function")
+    assert subtract(9, 4) == 5
+
+
+def test_multiply():
+    print("Testing multiply function")
+    assert multiply(5, 3) == 15
+
+
+def test_divide():
+    print("Testing divide function")
+    assert divide(6, 3) == 2
+
+
+def test_bank_set_initial_amount():
+    bank_account = BanckAccount(50)
+    assert bank_account.balance == 50
+
+
+def test_bank_default_amount():
+    bank_account = BanckAccount()
+    assert bank_account.balance == 0
+
+
+def test_withdraw():
+    bank_account = BanckAccount(50)
+    bank_account.withdraw(20)
+    assert bank_account.balance == 30
+
+
+def test_deposit():
+    bank_account = BanckAccount(50)
+    bank_account.deposit(30)
+    assert bank_account.balance == 80
+
+
+def test_collect_interest():
+    bank_account = BanckAccount(50)
+    bank_account.collect_interest()
+    assert round(bank_account.balance, 6) == 55
