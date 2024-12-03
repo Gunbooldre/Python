d63023910cce63c0ef4f86273eb3402534d3a7ab
diasmuratbayev <gunbooldre@gmail.com>
2024-12-03 20:06:25 +0500

    feat/adding fixture

--- fastProject/test/test_calculation.py
+++ fastProject/test/test_calculation.py
@@ -2,6 +2,16 @@ from calculation import add, subtract, multiply, divide, BanckAccount
 import pytest
 
 
+@pytest.fixture
+def zero_bank_account():
+    return BanckAccount()
+
+
+@pytest.fixture
+def bank_account():
+    return BanckAccount(50)
+
+
 @pytest.mark.parametrize("num1, num2, result", [
     (3, 2, 5),
     (7, 1, 8),
@@ -27,29 +37,24 @@ def test_divide():
     assert divide(6, 3) == 2
 
 
-def test_bank_set_initial_amount():
-    bank_account = BanckAccount(50)
+def test_bank_set_initial_amount(bank_account):
     assert bank_account.balance == 50
 
 
-def test_bank_default_amount():
-    bank_account = BanckAccount()
-    assert bank_account.balance == 0
+def test_bank_default_amount(zero_bank_account):
+    assert zero_bank_account.balance == 0
 
 
-def test_withdraw():
-    bank_account = BanckAccount(50)
+def test_withdraw(bank_account):
     bank_account.withdraw(20)
     assert bank_account.balance == 30
 
 
-def test_deposit():
-    bank_account = BanckAccount(50)
+def test_deposit(bank_account):
     bank_account.deposit(30)
     assert bank_account.balance == 80
 
 
-def test_collect_interest():
-    bank_account = BanckAccount(50)
+def test_collect_interest(bank_account):
     bank_account.collect_interest()
     assert round(bank_account.balance, 6) == 55
