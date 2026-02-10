af4ae37062e4c9ce0617f8f3d61d204a964fc7fc
diasmuratbayev <gunbooldre@gmail.com>
2026-02-10 12:06:29 +0500

    feat/ a little remind of decoratos

--- fastProject/app/utils/utils.py
+++ fastProject/app/utils/utils.py
@@ -1,6 +1,9 @@
 import asyncio
 import time
+from functools import wraps, lru_cache
+from typing import Callable, Coroutine
 
+from django.template.defaultfilters import yesno
 from passlib.context import CryptContext
 from fastapi import Query
 from app.schemas.schemas import PaginationParams
@@ -33,3 +36,87 @@ async def async_task():
     print("sync_task_async is done")
 
 
+def deco(func: Callable):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        res = func(*args, **kwargs)
+        end = time.time()
+        print(f'Tine for exec is {end - start}')
+        return res
+
+    return wrapper
+
+
+@deco
+def my_func():
+    time.sleep(3)
+    return 1
+
+
+def limit(temp: int):
+    def deco2(func: Callable):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            nonlocal temp
+            start = time.time()
+            res = func(*args, **kwargs)
+            end = time.time()
+            temp -= 1
+            if temp == 0:
+                print("ERROR MF")
+                return
+
+            print(f'Tine for exec is {end - start}')
+            return res
+
+        return wrapper
+    return deco2
+
+
+@limit(temp=2)
+def my_func2(time_sleep: int):
+    time.sleep(time_sleep)
+    return 1
+
+
+def deco_corutine(coroutine: Coroutine | Callable):
+    async def wrapper(*args, **kwargs):
+        return await coroutine(*args, **kwargs)
+    return wrapper
+
+
+@deco_corutine
+async def my_async_func():
+    print("Hello my friend")
+    await asyncio.sleep(3)
+
+# await my_async_func()
+
+
+@lru_cache
+def my_func_lru(x: int):
+    time.sleep(5)
+    return x * 2
+
+
+from contextlib import contextmanager
+
+
+def ctx_manager():
+    print("hello")
+    yield
+    print("end")
+
+
+with ctx_manager as m:
+    print("123")
+
+
+def log_func(func: Callable):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print(f"Start {func.__name__} args: {args} kwargs: {kwargs}")
+        res = func(*args, **kwargs)
+        print(f"End {func.__name__} args: {args} kwargs: {kwargs}")
+        return res
+    return wrapper
